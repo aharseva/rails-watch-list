c501f7d9473ed601bd4558b7017d6094e815f99d
Adriana Harseva <a.harseva@gmail.com>
2022-10-25 19:30:11 +0100

    all of saturday's work

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+# LISTS
+# get 'lists', to: 'lists#index'
+# get 'lists/new', to: 'lists#new', as: :new_list
+# post 'lists', to: 'lists#create'
+# get 'lists/:id', to: 'lists#show', as: :list
+
+# # BOOKMARKS
+# get 'bookmarks', to: 'bookmarks#new', as: :new_bookmark
+# post 'bookmarks', to: 'bookmarks#create'
+# delete 'bookmarks/:id', to: 'bookmarks#destroy'
+
+  root to: 'lists#index'
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create, :show]
+  end
+  resources :bookmarks, only: :destroy
+  resources :lists, only: :destroy, as: :list_delete
 end
